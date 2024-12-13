b0bbd16f25425bb2ec2d57c54b15e30adb26cc49
Ignacio Jorquera <ijorquera@alumnos.uai.cl>
2024-12-13 18:48:49 -0300

    Add number and symbol selection for password generation

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+// const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+const leters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
+let characters = []
 let passwordLength = 15
 let inputBox = document.getElementById("pwdLength")
+let numBtn = document.getElementById("num-btn")
+let symBtn = document.getElementById("sym-btn")
+let withNumbers = true
+let withSymbols = true
 
 function generatePassword() {
   let passwordOne = ""
   let passwordTwo = ""
+
+  if (withNumbers === false && withSymbols === false) {
+    characters = leters
+  } else if (withNumbers && withSymbols === false) {
+    characters = leters.concat(numbers)
+  } else if (withNumbers === false && withSymbols) {
+    characters = leters.concat(symbols)
+  } else {
+    characters = leters.concat(numbers, symbols)
+  }
+
   if (inputBox.value === "") {
     for (let i = 0; i < passwordLength; i++) {
       let randomIndexOne = Math.floor(Math.random() * characters.length)
@@ -23,4 +42,26 @@ function generatePassword() {
   }
   document.getElementById("pwdOne").textContent = passwordOne
   document.getElementById("pwdTwo").textContent = passwordTwo
+}
+
+function numbersSelector() {
+  if (numBtn.textContent === "Numbers ✅") {
+    numBtn.textContent = "Numbers ❌"
+    withNumbers = false
+  } else {
+    numBtn.textContent = "Numbers ✅"
+    withNumbers = true
+  }
+  return withNumbers
+}
+
+function symbolsSelector() {
+  if (symBtn.textContent === "Symbols ✅") {
+    symBtn.textContent = "Symbols ❌"
+    withSymbols = false
+  } else {
+    symBtn.textContent = "Symbols ✅"
+    withSymbols = true
+  }
+  return withSymbols
 }
